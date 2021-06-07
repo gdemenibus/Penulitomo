411b6313cf53ba29ff4ac71ccb9ae220d1df43b4
George H. de Menibus <g.f.l.hellouindemenibus@student.tudelft.nl>
2021-06-07 21:45:20 +0200

    Added Functionality
    It's alive

--- bot.py
+++ bot.py
@@ -1,26 +1,39 @@
 # bot.py
 import os
-
 import discord
+import demoji
 from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD = os.getenv("DISCORD_GUILD")
 
 
 client = discord.Client()
 
+ 	
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f"{client.user} has connected to Discord!")
     for guild in client.guilds:
         if guild.name == GUILD:
             break
     print(
-        f'{client.user} is connected to the following guild: \n'
-        f'{guild.name}(id: {guild.id})'
+        f"{client.user} is connected to the following guild: \n"
+        f"{guild.name}(id: {guild.id})"
     )
+def get_rehab_role(roles):
+    for x in roles:
+        if x.name == "Horny Rehab":
+            return x
+@client.event
+async def on_message(message):
+    emoji = demoji.findall(message.content) 
+    if "pleading face" in emoji.values():
+        print('Registered Horniness')
+        await message.channel.send("JAIL")
+        rehab = get_rehab_role(message.guild.roles)
+        await message.author.add_roles(rehab)
 client.run(TOKEN)
-
