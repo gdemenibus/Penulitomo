e3594019da6f13cab77322b3ab9dbaf989c67fb7
George H. de Menibus <g.f.l.hellouindemenibus@student.tudelft.nl>
2021-06-18 17:13:04 +0200

    CHORE: Refactored Penulitmo. Also added voicelines

--- bot.py
+++ bot.py
@@ -1,89 +1,100 @@
 # bot.py
 import os
-
+import json
 import demoji
 import discord
 from dotenv import load_dotenv
 
-load_dotenv()
 
 
-TOKEN = os.getenv("DISCORD_TOKEN")
-GUILD = os.getenv("DISCORD_GUILD")
-if TOKEN is None:
-    print("Token Missing")
-    exit(1)
-intent = discord.Intents.default()
-intent.members = True
-client = discord.Client(intents=intent)
-
-message_amount = 0
-
-
-@client.event
-async def on_ready():
-    print(f"{client.user} has connected to Discord!")
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-    print(
-        f"{client.user} is connected to the following guild: \n"
-        f"{guild.name}(id: {guild.id})"
-    )
-
-
-def get_rehab_role(roles):
-    for x in roles:
-        if x.name == "Horny Rehab":
-            return x
-
-
-async def emoji_check(message):
-    emoji = demoji.findall(message.content)
-    if message.guild is None:
-        await message.channel.send(
-            "Penultimo's loyalty is paramount. He does not take any bribes. If you want to bribe El Presidente, send your letters to @Gbus"
+class Penultimo(discord.Client):    
+    def __init__(self, guild):
+        intent = discord.Intents.default()
+        intent.members = True
+        super().__init__(intents = intent)
+        self.guild = guild
+        self.message_amount = 0
+ 
+    async def on_ready(self):
+        print(f"{self.user} has connected to Discord!")
+        for guild in self.guilds:
+            if guild.name == self.guild:
+                break
+        print(
+            f"{self.user} is connected to the following guild: \n"
+            f"{guild.name}(id: {guild.id})"
         )
-        return
-    if "pleading face" in emoji.values():
-        await message.channel.send(
-            message.author.mention + ", you're going straight to jail"
-        )
-        rehab = get_rehab_role(message.guild.roles)
-        await message.author.add_roles(rehab)
 
 
-async def message_count(message):
-    global message_amount
-    message_amount = message_amount + 1
-    if message_amount == 100:
-        guild = client.get_guild(message.guild.id)
-        message_amount = 0
-        for x in guild.roles:
+    def get_rehab_role(self, roles):
+        for x in roles:
             if x.name == "Horny Rehab":
-                for member in x.members:
-                    await member.remove_roles(x)
+                return x
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    await emoji_check(message)
-    await message_count(message)
+    async def emoji_check(self, message):
+        emoji = demoji.findall(message.content)
+        if message.guild is None:
+            await message.channel.send(
+                "Penultimo's loyalty is paramount. He does not take any bribes. If you want to bribe El Presidente, send your letters to @Gbus"
+            )
+            return
+        if "pleading face" in emoji.values():
+            await message.channel.send(
+                message.author.mention + ", you're going straight to jail"
+            )
+            rehab = self.get_rehab_role(message.guild.roles)
+            await message.author.add_roles(rehab)
 
 
-@client.event
-async def on_reaction_add(reaction, user):
-    if reaction.message.guild is None:
-        await reaction.message.channel.send(
-            "Penultimo is confused by your reaction. Are you trying to bribe Penultimo?"
-        )
-        return
-    emoji = demoji.findall(str(reaction.emoji))
-    if "pleading face" in emoji.values():
-        rehab = get_rehab_role(reaction.message.guild.roles)
-        await user.add_roles(rehab)
+    async def message_count(self, message):
+        self.message_amount = self.message_amount + 1
+        if self.message_amount == 100:
+            guild = self.get_guild(message.guild.id)
+            self.message_amount = 0
+            for x in guild.roles:
+                if x.name == "Horny Rehab":
+                    for member in x.members:
+                        await member.remove_roles(x)
 
 
-client.run(TOKEN)
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        await self.emoji_check(message)
+        await self.message_count(message)
+
+
+    async def on_reaction_add(self, reaction, user):
+        if reaction.message.guild is None:
+            await reaction.message.channel.send(
+                "Penultimo is confused by your reaction. Are you trying to bribe Penultimo?"
+            )
+            return
+        emoji = demoji.findall(str(reaction.emoji))
+        if "pleading face" in emoji.values():
+            rehab = self.get_rehab_role(reaction.message.guild.roles)
+            await user.add_roles(rehab)
+
+
+
+def main():
+    load_dotenv()
+
+    with open("quotes.json") as quotes:
+        quotes_json = json.load(quotes)
+    TOKEN = os.getenv("DISCORD_TOKEN")
+    GUILD = os.getenv("DISCORD_GUILD")
+    if TOKEN is None:
+        print("Token Missing")
+        exit(1)
+    penultimo = Penultimo(TOKEN)
+    penultimo.run(TOKEN)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
