1da3c130b1dd23c39de88a23fdb89300df1f58b2
Victor Roest <victor@xirion.net>
2021-06-18 18:16:15 +0200

    CHORE: Extracted quotes to json

---
+++ tools/lines_to_phrases.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Missing arguments")
+        exit(1)
+
+    orig = sys.argv[1]
+    out = sys.argv[2]
+
+    with open(orig, 'r') as orig_file:
+        with open(out, 'w') as out_file:
+            out_file.write("[\n")
+            for line in orig_file.readlines():
+                line = line[1:-2]
+                out_file.write(f"{{ \"phrase\": \"{line}\", \"keywords\": [\"testing\"] }},\n")
+            out_file.write("]")
+
+
